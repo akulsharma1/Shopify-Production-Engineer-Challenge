831c178b1a1548953489d8c2ef32700561255148
akulsharma1 <akulsharma02@gmail.com>
2022-01-07 19:54:25 -0800

    Added another test func

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"shopifyproductionengineertest/inventory"
 	"shopifyproductionengineertest/managerfunctions"
 )
 
 func main() {
 	managerfunctions.ReadProducts()
+	managerfunctions.CreateInventoryItem("test", []inventory.Variant{inventory.Variant{VariantID: 1, VariantName: "test1", Size: "test", Image: "test", Quantity: 1, Price: 1.0}})
 }
