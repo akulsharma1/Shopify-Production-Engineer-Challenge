82a94fc964cd3b5374ed4d5f41234a3706a2f2a2
akulsharma1 <akulsharma02@gmail.com>
2022-01-07 19:54:32 -0800

    Added create inventory item

--- managerFunctions/createInventoryItem.go
+++ managerFunctions/createInventoryItem.go
@@ -1,5 +1,25 @@
 package managerfunctions
 
-func CreateInventoryItem(productName string) {
-
+import (
+	"encoding/json"
+	"io/ioutil"
+	"shopifyproductionengineertest/inventory"
+)
+func CreateInventoryItem(productName string, variants []inventory.Variant) bool {
+	worked, currentProdList := ReadProducts()
+	if worked {
+		
+		var newProd = inventory.Product{
+			ProductName: productName,
+			Variants:    variants,
+		}
+		
+		currentProdList.Products = append(currentProdList.Products, newProd)
+		file, _ := json.MarshalIndent(currentProdList, "", "	")
+		err := ioutil.WriteFile("./inventory/inventory.json", file, 0644)
+		return err==nil
+	} else {
+		return false
+	}
+	
 }
