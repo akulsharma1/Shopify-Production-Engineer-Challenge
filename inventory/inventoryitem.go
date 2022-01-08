65ea45f749709b552447f0a5b1f289c1e53e13f6
akulsharma1 <akulsharma02@gmail.com>
2022-01-07 19:54:18 -0800

    Adjusted json format of products

--- inventory/inventoryitem.go
+++ inventory/inventoryitem.go
@@ -1,16 +1,18 @@
 package inventory
 
 type Product struct {
-	ProductName string `json:"productName"`
-	Sizes       []struct {
-		SizeID   int    `json:"sizeID"`
-		Size     string `json:"size"`
-		Name     string `json:"name"`
-		Image    string `json:"image"`
-		Quantity int    `json:"quantity"`
-	} `json:"sizes"`
+	ProductName string    `json:"productName"`
+	ProductID   string    `json:"productID"`
+	Variants    []Variant `json:"variants"`
 }
-
+type Variant struct {
+	VariantID   int     `json:"variantID"`
+	VariantName string  `json:"variantName"`
+	Size        string  `json:"size"`
+	Image       string  `json:"image"`
+	Quantity    int     `json:"quantity"`
+	Price       float64 `json:"price"`
+}
 type ProductList struct {
 	Products []Product `json:"products"`
 }
