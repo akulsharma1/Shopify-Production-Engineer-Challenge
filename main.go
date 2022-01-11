d1e85f12996dcc569b1fb316d4434be5fd55e300
akulsharma1 <akulsharma02@gmail.com>
2022-01-10 22:50:31 -0800

    Adjusted types

--- main.go
+++ main.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	managerfunctions.ReadProducts()
-	managerfunctions.CreateInventoryItem("test", []inventory.Variant{inventory.Variant{VariantID: 1, VariantName: "test1", Size: "test", Image: "test", Quantity: 1, Price: 1.0}})
+	managerfunctions.CreateInventoryItem("test", []inventory.Variant{inventory.Variant{VariantID: "1", VariantName: "test1", Size: "test", Image: "test", Quantity: 1, Price: 1.0}})
 }
