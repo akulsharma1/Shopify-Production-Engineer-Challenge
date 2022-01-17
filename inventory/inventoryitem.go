cfd2919a1943937f88bc4f3b4e9c60d086911ba6
akulsharma1 <akulsharma02@gmail.com>
2022-01-16 18:29:31 -0800

    Adjusted inventory item

--- inventory/inventoryitem.go
+++ inventory/inventoryitem.go
@@ -1,18 +1,26 @@
 package inventory
 
 type Product struct {
-	ProductName string    `json:"productName"`
-	ProductID   string    `json:"productID"`
-	Variants    []Variant `json:"variants"`
+	ProductName string `bson:"productName" json:"productName"`
+	ProductID   string `json:"productID" bson:"productID"`
+	Variants1   []struct {
+		VariantID   string  `json:"variantID" bson:"variantID"`
+		VariantName string  `json:"variantName" bson:"variantName"`
+		Size        string  `json:"size" bson:"size"`
+		Image       string  `json:"image" bson:"image"`
+		Quantity    int     `json:"quantity" bson:"quantity"`
+		Price       float64 `json:"price" bson:"price"`
+	} `json:"variants" bson:"variants"`
+	Variants []Variant `json:"variants2" bson:"variants2"`
 }
 type Variant struct {
-	VariantID   string  `json:"variantID"`
-	VariantName string  `json:"variantName"`
-	Size        string  `json:"size"`
-	Image       string  `json:"image"`
-	Quantity    int     `json:"quantity"`
-	Price       float64 `json:"price"`
+	VariantID   string  `json:"variantID" bson:"variantID"`
+	VariantName string  `json:"variantName" bson:"variantName"`
+	Size        string  `json:"size" bson:"size"`
+	Image       string  `json:"image" bson:"image"`
+	Quantity    int     `json:"quantity" bson:"quantity"`
+	Price       float64 `json:"price" bson:"price"`
 }
 type ProductList struct {
-	Products []Product `json:"products"`
+	Products []Product `json:"products" bson:"products"`
 }
