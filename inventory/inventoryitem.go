9caba969ce4c35da674151549a7ef2d0449271e4
akulsharma1 <akulsharma02@gmail.com>
2022-01-17 20:52:37 -0800

    Added some stuff

--- inventory/inventoryitem.go
+++ inventory/inventoryitem.go
@@ -1,19 +1,28 @@
 package inventory
 
 type Product struct {
-	ProductName string `bson:"productName" json:"productName"`
-	ProductID   string `json:"productID" bson:"productID"`
-	Variants1   []struct {
-		VariantID   string  `json:"variantID" bson:"variantID"`
-		VariantName string  `json:"variantName" bson:"variantName"`
-		Size        string  `json:"size" bson:"size"`
-		Image       string  `json:"image" bson:"image"`
-		Quantity    int     `json:"quantity" bson:"quantity"`
-		Price       float64 `json:"price" bson:"price"`
-	} `json:"variants" bson:"variants"`
-	Variants []Variant `json:"variants2" bson:"variants2"`
+	ProductName string    `bson:"productName" json:"productName"`
+	ProductID   string    `json:"productID" bson:"productID"`
+	Variants    []Variant `json:"variants" bson:"variants"`
 }
 type Variant struct {
+	VariantID   string  `json:"variantID" bson:"variantID"`
+	VariantName string  `json:"variantName" bson:"variantName"`
+	Size        string  `json:"size" bson:"size"`
+	Quantity    int     `json:"quantity" bson:"quantity"`
+	Price       float64 `json:"price" bson:"price"`
+}
+type ProductList struct {
+	Products []Product `json:"products" bson:"products"`
+}
+
+// fiber stuff
+//
+//
+//
+
+type FiberAddVariant struct {
+	ProductID   string  `json:"productID"`
 	VariantID   string  `json:"variantID" bson:"variantID"`
 	VariantName string  `json:"variantName" bson:"variantName"`
 	Size        string  `json:"size" bson:"size"`
@@ -21,6 +30,12 @@ type Variant struct {
 	Quantity    int     `json:"quantity" bson:"quantity"`
 	Price       float64 `json:"price" bson:"price"`
 }
-type ProductList struct {
-	Products []Product `json:"products" bson:"products"`
+
+type FiberGetProductID struct {
+	ProductID string `json:"productID"`
+}
+
+type FiberGetVariantInfo struct {
+	ProductID string `json:"productID"`
+	VariantID string `json:"variantID"`
 }
